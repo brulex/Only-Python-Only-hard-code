0dd7ad2d6aabd10eb72b29532e60970b4099271f
Anton Trukhan <deko71dekobraz@gmail.com>
2018-12-02 19:22:43 +0200

    Change site style and implement lessons.service module

--- angularweb/src/app/services/lesson.service.ts
+++ angularweb/src/app/services/lesson.service.ts
@@ -1,28 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonService {
 
-  constructor() { }
-
-  getLossonsData() {
-    return [
-      {title: "Возможности языка Python", content_id: "1"},
-      {title: "Загрузка и установка Python", content_id: "2"},
-      {title: "Первая программа. Знакомство со средой разработки IDLE", content_id: "3"},
-      {title: "Синтаксис", content_id: "4"},
-      {title: "Почему моя программа не работает?", content_id: "5"},
-      {title: "Условный оператор if", content_id: "6"},
-      {title: "Циклы", content_id: "/7"},
-      {title: "Ключевые слова, встроенные функции", content_id: "/8"},
-      {title: "Числа", content_id: "/9"},
-      {title: "Строки (часть 1, часть 2, форматирование)", content_id: "/10"},
-    ];
+  constructor(private http: HttpClient) {
   }
 
-  getLessonContent(content_id: string) {
-    return '<h6>Этот сайт призван помочь начинающим и чайникам научиться программировать на python 3. Также здесь можно подробнее узнать об особенностях функционирования этого языка.</h6>';
+  getLossonsData(): Observable<any> {
+    return this.http.get<any>('http://127.0.0.1:8000/api/lessons/');
+  }
+
+  getLessonContent(content_id: string): Observable<any> {
+    return this.http.get<any>('http://127.0.0.1:8000/api/lessons/' + content_id + '/');
   }
 }
