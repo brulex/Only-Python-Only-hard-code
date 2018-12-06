9ba549bc05922e4637491e4030f35048ecd7bcfe
Anton Trukhan <deko71dekobraz@gmail.com>
2018-12-06 20:54:16 +0200

    Update Map component

--- angularweb/src/app/map/map.component.ts
+++ angularweb/src/app/map/map.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Content} from "../models/content";
-import {MapService} from "../services/map.service";
+import {Content, CourseContent} from "../models/content";
 import {SiteNavigator} from "../site.navigator";
+import {LessonService} from "../services/lesson.service";
+import {CourseService} from "../services/course.service";
 import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-map',
@@ -10,33 +12,62 @@ import {Router} from "@angular/router";
   styleUrls: ['./map.component.css', '../app.component.css']
 })
 export class MapComponent implements OnInit {
-  mapCoursesList: Content[];
+  mapCoursestList: CourseContent[];
   mapLesssonsList: Content[];
   siteNavigator: SiteNavigator;
 
-  constructor(private mapService: MapService,
+  constructor(private lessonService: LessonService,
+              private courseService: CourseService,
               private router: Router) {
     this.siteNavigator = new SiteNavigator(router);
-    this.mapCoursesList = [];
+    this.mapCoursestList = [];
     this.mapLesssonsList = [];
   }
 
   ngOnInit() {
-    let map = this.mapService.getSiteMap();
-    map.data.courses.forEach(element => {
-      this.mapCoursesList.push
-      ({
-        title: element.title,
-        content_id: element.content_id
+
+    this.courseService.getCoursesList().subscribe(value => {
+      value.forEach(element => {
+        this.mapCoursestList.push
+        ({
+          courseData: {
+            title: element.name,
+            content_id: element.id
+          },
+          articles: null
+        });
+          let content_id = this.mapCoursestList.length - 1;
+          this.courseService.getCourseContent(this.mapCoursestList[content_id].courseData.content_id).subscribe(value => {
+              this.mapCoursestList[content_id].articles = [];
+              value.forEach(element => {
+                this.mapCoursestList[content_id].articles.push
+                ({
+                  title: element.title,
+                  content_id: element.id
+                });
+              });
+            }, error => {
+              alert(JSON.parse(error));
+            }
+          );
+
       });
+    }, error => {
+      alert(JSON.parse(error));
     });
-    map.data.lessons.forEach(element => {
-      this.mapLesssonsList.push
-      ({
-        title: element.title,
-        content_id: element.content_id
-      });
-    });
+
+    this.lessonService.getLossonsList().subscribe(value => {
+        value.forEach(element => {
+          this.mapLesssonsList.push
+          ({
+            title: element.title,
+            content_id: element.id
+          });
+        });
+      }, error => {
+        alert(JSON.parse(error));
+      }
+    );
   }
 
 }
--- angularweb/src/app/models/content.ts
+++ angularweb/src/app/models/content.ts
@@ -2,3 +2,8 @@ export interface Content {
   title: string;
   content_id: string;
 }
+
+export interface CourseContent {
+  courseData: Content;
+  articles: Content[];
+}
