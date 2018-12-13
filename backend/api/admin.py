56497e6abcec8dd4e4889d49c313614f0230fb4a
Anton Trukhan <deko71dekobraz@gmail.com>
2018-12-13 20:06:55 +0200

    Add model for questions

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Articles, Courses
+from .models import Articles, Courses, Question, Answer
+from django.db import models
+from django.forms import CheckboxSelectMultiple
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    filter_horizontal = ('answers_list', )
 
 # Register your models here.
 admin.site.register(Courses)
 admin.site.register(Articles)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+
--- backend/api/models.py
+++ backend/api/models.py
@@ -56,4 +56,40 @@ class Articles(models.Model):
         return self.title
 
 
+class Answer(models.Model):
 
+    answer = models.CharField(
+        max_length=255,
+        null=False
+    )
+
+    def __str__(self):
+        return self.answer
+
+
+class Question(models.Model):
+
+    question = models.CharField(
+        max_length=255,
+        null=False
+    )
+
+    answers_list = models.ManyToManyField(Answer)
+
+    true_answer = models.ForeignKey(
+        to=Answer,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="true_answer"
+    )
+
+    course_id = models.ForeignKey(
+        to=Courses,
+        on_delete=models.PROTECT,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.question
