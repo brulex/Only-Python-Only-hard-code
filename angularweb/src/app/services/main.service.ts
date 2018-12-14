3f6e3c2d7393235bc429aa133348e181115a56f5
Anton Trukhan <deko71dekobraz@gmail.com>
2018-12-15 00:26:53 +0200

    Add method to call api method for search

--- angularweb/src/app/services/main.service.ts
+++ angularweb/src/app/services/main.service.ts
@@ -1,11 +1,16 @@
 import {Injectable} from '@angular/core';
-
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  articlesSearch(search_title: string): Observable<any> {
+    return this.http.get<any>('http://127.0.0.1:8000/api/search/' + search_title + '/');
   }
 }
