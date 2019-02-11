c978c031e3c15f0eaa53c08c6814b026ae923f49
Anton Trukhan <deko71dekobraz@gmail.com>
2019-02-11 19:21:17 +0200

    Remove unused imports

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Articles, Courses, Question, Answer
-from django.db import models
-from django.forms import CheckboxSelectMultiple
 
 
 class QuestionAdmin(admin.ModelAdmin):
